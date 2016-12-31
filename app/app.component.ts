ca35b5b3db61cadf6c9ff56054a4d0b2e54fab4d
zhouyili <623109799@qq.com>
2016-12-31 22:07:33 +0800

    将英雄独立为一个Class

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import {Hero} from "./hero";
 
 @Component({
   selector: 'my-app',
-  template: `<h1>{{title}}</h1><h2>{{hero}} Details</h2>`,
+  template: `<h1>{{title}}</h1><h2>{{hero.name}} Details</h2>`,
 })
 export class AppComponent  {
   title = 'Tour Of Heroes';
-  hero = 'Windstorm'; }
+  hero : Hero={
+    id:1,
+    name:'Windstorm'
+  } ; }
