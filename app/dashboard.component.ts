f6cda4ad79f6e82571414aa0a25a3b34134b24cb
zhouyili <623109799@qq.com>
2017-01-16 21:59:17 +0800

    添加DashBoard的显示内容，4个英雄

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,6 +1,16 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {HeroService} from "./hero.service";
+import {Hero} from "./hero";
 @Component({
+    moduleId:module.id,
     selector:'my-dashboard',
-    template:'<h3>My Dashboard</h3>'
+    templateUrl:'dashboard.component.html'
 })
-export class DashboardComponent{}
+export class DashboardComponent implements OnInit{
+    private heroes:Hero[]=[];
+    ngOnInit(): void {
+       this.heroService.getHeroes().then(heroes=>this.heroes=heroes.slice(0,4));
+    }
+    constructor(private heroService:HeroService){ }
+
+}
